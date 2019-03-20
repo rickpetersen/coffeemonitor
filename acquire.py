9f279f2146e6f2df6843c99aeb310f160d6edad5
Rick Petersen <rickpetersen@gmail.com>
2019-03-20 00:04:05 -0500

    removed loop because we are running as a cron job

--- acquire.py
+++ acquire.py
@@ -20,15 +20,14 @@ try:
     camera = PiCamera()
     #camera.rotation = 180
     #camera.start_preview(fullscreen=False, window = (100, 20, 640, 480))
-    while True:
-        local_file_name = 'coffee{date:%Y%m%d%H%M%S}.jpg'.format( date=datetime.datetime.now() )
-        full_path_to_file = '/home/pi/Desktop/%s' % local_file_name
-        camera.capture(full_path_to_file)
-        sleep(2)
-        blob_service.create_blob_from_path(CONTAINER_NAME, local_file_name, full_path_to_file)
-        #camera.stop_preview()
-        print('Pushed %s to blob storage' % local_file_name);
-        os.remove(full_path_to_file)
+    local_file_name = 'coffee{date:%Y%m%d%H%M%S}.jpg'.format( date=datetime.datetime.now() )
+    full_path_to_file = '/home/pi/Desktop/%s' % local_file_name
+    camera.capture(full_path_to_file)
+    sleep(2)
+    blob_service.create_blob_from_path(CONTAINER_NAME, local_file_name, full_path_to_file)
+    #camera.stop_preview()
+    print('Pushed %s to blob storage' % local_file_name);
+    os.remove(full_path_to_file)
         
 except Exception as e:
     #camera.stop_preview()
